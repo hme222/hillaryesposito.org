a14ce7a913a46312cead4aae1050d607159ee475
Hillary Esposito <espositohillary@gmail.com>
2026-01-19 15:56:10 -0500

    upload issue fixes

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -14,4 +14,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals(console.log);
+reportWebVitals();
--- my-app/src/reportWebVitals.js
+++ my-app/src/reportWebVitals.js
@@ -1,12 +1,12 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+import { getCLS, getFID, getLCP, getFCP, getTTFB } from "web-vitals";
+
+const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
+  if (onPerfEntry && typeof onPerfEntry === "function") {
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getLCP(onPerfEntry);
+    getFCP(onPerfEntry);
+    getTTFB(onPerfEntry);
   }
 };
 
